9fe70b777b6ec5b5ee76264bc392c04347cea828
quou <georgeashleylamb@gmail.com>
2022-08-22 15:58:33 +1000

    Basic build menu.

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -2,25 +2,5 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-	public Material[] OriginalMaterials {  get; private set; }
 
-	[SerializeField] bool built = false;
-
-	public void Start()
-	{
-		OriginalMaterials = (Material[])GetComponent<MeshRenderer>().materials.Clone();
-
-		if (!built)
-        {
-			GetComponent<MeshRenderer>().materials = new Material[] { BuildingManager.Instance.ghostMaterial };
-		}
-	}
-
-	public void Build()
-    {
-		if (built) { return; }
-
-		GetComponent<MeshRenderer>().materials = (Material[])OriginalMaterials.Clone();
-		built = true;
-    }
 }
--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -2,29 +2,29 @@ using UnityEngine;
 
 public class BuildingManager : MonoBehaviour
 {
-    public LayerMask buildingLayer;
-    public Material ghostMaterial;
-    [SerializeField] Camera mainCamera;
+	public LayerMask buildingLayer;
+	public Material ghostMaterial;
+	[SerializeField] Camera mainCamera;
 
-    static public BuildingManager Instance {  get; private set; }
+	static public BuildingManager Instance {  get; private set; }
 
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        } else
-        {
-            Destroy(this);
-        }
-    }
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		} else
+		{
+			Destroy(this);
+		}
+	}
 
-    void Update()
-    {
-        RaycastHit hit;
-        if (Input.GetMouseButtonUp(0) && Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, buildingLayer)) {
-            Building building = hit.collider.gameObject.GetComponent<Building>();
-            building.Build();
-        }
-    }
+	void Update()
+	{
+		RaycastHit hit;
+		if (Input.GetMouseButtonUp(0) && Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, buildingLayer)) {
+			Ghost building = hit.collider.gameObject.GetComponent<Ghost>();
+			building.ShowBuildMenu();
+		}
+	}
 }
