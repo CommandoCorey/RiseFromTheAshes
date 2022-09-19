b86cc5c32b7a32533ddea4ca50f80f9c1c878f6c
Corey <CommandoCorz@gmail.com>
2022-09-19 11:19:23 +1000

    fix resourcebuilding.cs and resourcemanager.cs

--- Assets/Scripts/Buildings/ResourceBuilding.cs
+++ Assets/Scripts/Buildings/ResourceBuilding.cs
@@ -3,16 +3,10 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public enum ResourceNumber
-{
-    Steel
-}
-
-
 public class ResourceBuilding : Building
 {
     //public int ResourceToGain;
-    public ResourceNumber resourceToAdd;
+    public ResourceType resourceToAdd;
     public int quantityToAdd = 10;
     public float timePerIncerement = 1;
 
@@ -37,7 +31,7 @@ public class ResourceBuilding : Building
 
     private void IncrementResource()
     {
-        resources.AddResource((int)resourceToAdd, quantityToAdd);
+        resources.AddResource(resourceToAdd, quantityToAdd);
         floatingLabel.SetActive(true);
         floatingLabel.GetComponent<FloatingResourceLabel>().Begin(quantityToAdd);
         Invoke("IncrementResource", timePerIncerement);
