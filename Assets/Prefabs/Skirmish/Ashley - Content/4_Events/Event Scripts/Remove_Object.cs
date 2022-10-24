13a944c99dca3e5faaf29fc503bd126dce89b67c
ashley <ashleyfj77760@gmail.com>
2022-10-24 11:08:37 +1100

    Fix and making for new events

--- Assets/Prefabs/Skirmish/Ashley - Content/4_Events/Event Scripts/Remove_Object.cs
+++ Assets/Prefabs/Skirmish/Ashley - Content/4_Events/Event Scripts/Remove_Object.cs
@@ -8,22 +8,32 @@ public class Remove_Object : MonoBehaviour
 
     [Space(10)]
 
+    BoxCollider bc;
+
     public List<GameObject> SpawnObjects = new List<GameObject>();
     public GameObject Remove;
 
     // Start is called before the first frame update
-    void OnTriggerEnter(Collider other)
+    void MyOnTrigger(Collider other)
     {
         //entering the trigger
-        Debug.Log("Spawning Object");
-        if (((1 << other.gameObject.layer) & unitLayerMask) == 0)
+        Debug.Log("Removing Object");
+        foreach (var g in SpawnObjects)
         {
-            foreach (var g in SpawnObjects)
-            {
-                g.SetActive(false);
-            }
-
-            Remove.SetActive(false);
+            g.SetActive(false);
         }
+
+        Remove.SetActive(false);
+
+    }
+    private void OnEnable()
+    {
+        bc = GetComponent<BoxCollider>();
+    }
+
+    private void FixedUpdate()
+    {
+        var hits = Physics.OverlapBox(bc.bounds.min, (bc.bounds.max - bc.bounds.min) * 0.5f, Quaternion.identity, unitLayerMask);
+        foreach (var hit in hits) { MyOnTrigger(hit); }
     }
 }
--- Assets/Prefabs/Skirmish/Ashley - Content/4_Events/Event Scripts/Spawn_Object.cs
+++ Assets/Prefabs/Skirmish/Ashley - Content/4_Events/Event Scripts/Spawn_Object.cs
@@ -8,22 +8,31 @@ public class Spawn_Object : MonoBehaviour
 
     [Space(10)]
 
+    BoxCollider bc;
+
     public List<GameObject> SpawnObjects = new List<GameObject> ();
     public GameObject Remove;
 
     // Start is called before the first frame update
-    void OnTriggerEnter(Collider other)
+    void MyOnTrigger(Collider other)
     {
         //entering the trigger
         Debug.Log("Spawning Object");
-        if (((1 << other.gameObject.layer) & unitLayerMask) == 0)
-        {
             foreach (var g in SpawnObjects) {
                 g.SetActive(true);
             }
 
             Remove.SetActive(false);
-        }
 
+    }
+    private void OnEnable()
+    {
+        bc = GetComponent<BoxCollider>();
+    }
+
+    private void FixedUpdate()
+    {
+        var hits = Physics.OverlapBox(bc.bounds.min, (bc.bounds.max - bc.bounds.min) * 0.5f, Quaternion.identity, unitLayerMask);
+        foreach (var hit in hits) { MyOnTrigger(hit); }
     }
 }
