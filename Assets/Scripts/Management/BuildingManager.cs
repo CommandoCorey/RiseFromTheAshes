0680c351cf1799c795926f01f88959d39c9e7a0d
George Lamb <georgeashleylamb@gmail.com>
2022-11-07 15:41:33 +1100

    Remove an unnecessary raycast from the building manager and update the ghost prefab.

--- Assets/Scripts/Management/BuildingManager.cs
+++ Assets/Scripts/Management/BuildingManager.cs
@@ -27,15 +27,7 @@ public class BuildingManager : MonoBehaviour
 		if (Input.GetMouseButtonUp(0))
 		{
 
-			if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, buildableLayer))
-			{
-				Ghost building = hit.collider.gameObject.GetComponent<Ghost>();
-
-				if (building)
-				{
-					building.ShowBuildMenu();
-				}
-			} else if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, buildingLayer))
+			if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, buildingLayer))
 			{
 				Debug.Log(hit.collider.gameObject.name);
 				Building building = hit.collider.gameObject.GetComponent<Building>();
